6ee16d3f2a5a478b8201d60a091b9b9b3097aed3
Trey Decker <treydecker75@gmail.com>
2019-12-08 13:49:51 -0700

    refactored the DAL.js

--- dal.js
+++ dal.js
@@ -1,25 +1,30 @@
 const { Client } = require('pg');
 
-const client = new Client({
-    user: "mgmt_user",
-    password: 'acd955eb-3da5-46a4-9d4e-e987b512ab1b',
-    host: "127.0.0.1",
-    database: "mgmt",
-    port: 5432
-});
-client.connect(err => {
-    if (err) {
-        console.log('PG CONNECTION ERROR: ', err.stack);
-        process.exit(1);
-    }
-    console.log('PG CONNECTED SUCCESSFULLY');
-});
+const HomeDAL = {
+    init: () => {
+        this.client = new Client({
+            user: "mgmt_user",
+            password: 'acd955eb-3da5-46a4-9d4e-e987b512ab1b',
+            host: "127.0.0.1",
+            database: "mgmt",
+            port: 5432
+        });
+        this.client.connect(err => {
+            if (err) {
+                console.log('PG CONNECTION ERROR: ', err.stack);
+                process.exit(1);
+            }
+            console.log('PG CONNECTED SUCCESSFULLY');
+        });
+    },
 
-const getHomes = (request, response) => {
-    client.query("SELECT * FROM mgmt.homes", (error, results) => {
-        if (error) throw error;
-        response.status(200).send(results.rows);
-    })
+    getHomes: (req, response) => {
+        this.client.query("SELECT * FROM mgmt.homes", (error, results) => {
+            if (error) throw error;
+            response.status(200).send(results.rows);
+        })
+    } 
 }
 
-module.exports = { getHomes };
+
+module.exports = HomeDAL;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const fastify = require('fastify')({ logger: true });
 const db = require('./dal.js');
 
+db.init();
+
+console.log('db', db);
+
 fastify.get('/healthcheck', async (req, resp) => {
     return {
         status: 'ok'
