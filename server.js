fa31ab515dc69e6db1f73d78ae6dfabb391dc59c
Trey Decker <treydecker75@gmail.com>
2019-12-08 11:47:46 -0700

    added package.json and updated server.js to start using routes

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const fastify = require('fastify')({ logger: true });
 
-fastify.get('/healtcheck', async(req, resp) => {
+fastify.get('/healthcheck', async(req, resp) => {
     return {
         status: 'ok'
     };
 });
+
+const start = async () => {
+    const PORT = process.env.PORT || 3000;
+    try {
+        await fastify.listen(PORT);
+        fastify.log.info(`Server listening on ${PORT}`);
+    } catch (error) {
+        fastify.log.error(error);
+        process.exit(1);
+    }
+}
+
+start();
